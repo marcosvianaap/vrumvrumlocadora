dd42474d938a4ad388f95ca66284c2123ef1b1cc
José Mateus Amaral de Miranda <50233573+josemateusamaral@users.noreply.github.com>
2024-11-14 03:39:49 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ app.static_folder = 'static'
 app.secret_key = 'chaveSecretaParaCriptografia'
 #bcrypt = Bcrypt(app)
 
-#rotas para entrar e sair do sistema
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -22,20 +23,12 @@ def index():
 def administrador():
     return render_template("administrador.html")
 
+
+
+#ROTAS PARA GERENCIAMENTO DE FUNCIONARIOS
 @app.route("/administrador/funcionarios")
 def pesquisar_funcionario():
     return render_template("pesquisar_funcionario.html")
-
-
-# Rota de gerenciamento de veiculos
-@app.route("/administrador/veiculos")
-def gerenciar_veiculos():
-    return render_template("geren_veic.html")
-
-#Rota para cadastrar um novo veiculo
-@app.route("/administrador/veiculos/cadastro")
-def criar_veiculos():
-    return render_template("criar_veiculo.html")
 
 @app.route("/administrador/funcionarios/criarFuncionario")
 def criar_funcionario():
@@ -43,6 +36,114 @@ def criar_funcionario():
     return render_template("criar_funcionario.html", script_url=script_url)
 
 
+
+
+
+
+
+# ROTAS PARA GERENCIAMENTO DE VEICULOS ----------------------------
+
+#rota para a tela de listagem de veiculos
+@app.route("/administrador/veiculos")
+def gerenciar_veiculos():
+
+    conn = bd.connect_to_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id,Ano_Aquisicao,Placa,RENAVAM,Modelo,Marca,Ano_Fabricacao,Cor,Tipo_Combustivel,Valor_Locacao_Dia,Status FROM Veiculo")
+    veiculosRaw = cursor.fetchall()
+    veiculos = []
+    colunas = ["id","Ano_Aquisicao","Placa","RENAVAM","Modelo","Marca","Ano_Fabricacao","Cor","Tipo_Combustivel","Valor_Locacao_Dia","Status"]
+    for i in veiculosRaw:
+        veiculo = {}
+        for index,j in enumerate(i):
+            veiculo[colunas[index]] = j
+        veiculos.append(veiculo)
+    print(veiculos)
+    conn.close()
+
+    return render_template("geren_veic.html",veiculos=veiculos)
+
+#Rota para a tela de cadastro de veiculos
+@app.route("/administrador/veiculos/frontend_criar_veiculo")
+def frontend_criar_veiculos():
+    return render_template("criar_veiculo.html")
+
+#Rota para cadastrar um novo veiculo
+@app.route("/administrador/veiculos/backend_criar_veiculo",methods=['POST'])
+def backend_criar_veiculo():
+
+    modelo = request.form["modelo"]
+    cor = request.form["cor"]
+    marca = request.form["marca"]
+    placa = request.form["placa"]
+    renavam = request.form["renavam"]
+    anoFabricacao = request.form["anoFabricacao"]
+    anoAquisicao = request.form["anoAquisicao"]
+    valorAlocacao = request.form["valorAlocacao"]
+    tipoCombustivel = request.form["tipoCombustivel"]
+    status = request.form["status"]
+
+    conn = bd.connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO Veiculo (Ano_Aquisicao,Placa,RENAVAM,Modelo,Marca,Ano_Fabricacao,Cor,Tipo_Combustivel,Valor_Locacao_Dia,Status) VALUES (?,?,?,?,?,?,?,?,?,?)",(anoAquisicao,placa,renavam,modelo,marca,anoFabricacao,cor,tipoCombustivel,valorAlocacao,status))
+    conn.commit()
+
+    return redirect(url_for("gerenciar_veiculos")) 
+
+#Rota para a tela de editar um veiculo
+@app.route("/administrador/veiculos/frontend_editar_veiculo",methods=['POST'])
+def frontend_editar_veiculo():
+    veiculo = request.form["veiculo"]
+
+    conn = bd.connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id,Ano_Aquisicao,Placa,RENAVAM,Modelo,Marca,Ano_Fabricacao,Cor,Tipo_Combustivel,Valor_Locacao_Dia,Status FROM Veiculo WHERE id = ?",(veiculo))
+    veiculoRaw = cursor.fetchone()
+    veiculoDict = {}
+    colunas = ["id","Ano_Aquisicao","Placa","RENAVAM","Modelo","Marca","Ano_Fabricacao","Cor","Tipo_Combustivel","Valor_Locacao_Dia","Status"]
+    for index,j in enumerate(veiculoRaw):
+        veiculoDict[colunas[index]] = j
+    conn.close()
+
+    return render_template("editar_veiculo.html",veiculo=veiculoDict)
+
+#Rota para salvar a edicao de um veiculo
+@app.route("/administrador/veiculos/backend_editar_veiculo",methods=['POST'])
+def backend_editar_veiculo():
+
+    veiculo = request.form["veiculo"]
+    modelo = request.form["modelo"]
+    cor = request.form["cor"]
+    marca = request.form["marca"]
+    placa = request.form["placa"]
+    renavam = request.form["renavam"]
+    anoFabricacao = request.form["anoFabricacao"]
+    anoAquisicao = request.form["anoAquisicao"]
+    valorAlocacao = request.form["valorAlocacao"]
+    tipoCombustivel = request.form["tipoCombustivel"]
+    status = request.form["status"]
+
+    conn = bd.connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+                   UPDATE Veiculo 
+                   SET Ano_Aquisicao = ?, Placa = ?, RENAVAM = ?, Modelo = ?, Marca = ?, Ano_Fabricacao = ?, Cor = ?, Tipo_Combustivel = ?, Valor_Locacao_Dia = ?, Status = ? 
+                   WHERE id = ? 
+                   """,(anoAquisicao,placa,renavam,modelo,marca,anoFabricacao,cor,tipoCombustivel,valorAlocacao,status,veiculo))
+    conn.commit()
+
+    return redirect(url_for("gerenciar_veiculos")) 
+
+
+
+
+
+
+
+# ROTAS PARA ENTRAR E SAIR DO SISTEMA ----------------------------------
+
+#rota para a tela de login do sistema
 @app.route('/tela_login')
 def tela_login():
     if 'statusLogin' in session:
@@ -53,6 +154,7 @@ def tela_login():
     else:
         return render_template("tela_login.html")
 
+#rota para entrar no sistema
 @app.route("/entrar",methods=['POST'])
 def processoEntrar():
     if not bd.autenticarUsuario(request.form):
@@ -64,11 +166,15 @@ def processoEntrar():
         session['usuario'] = request.form['usuario']
         return redirect(url_for("index"))
 
-@app.route("/processo/sair")
+#rota para sair do sistema
+@app.route("/sair")
 def processoSair():
     if 'usuario' in session:
         del session['usuario']
     return redirect(url_for('index'))
+
+
+
 
 
 if __name__ == '__main__':
